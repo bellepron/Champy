ace9d82ac7214effc8ebcb7790932bf1cd088dd2
caglar <caglarkutluyigit@gmail.com>
2022-01-15 04:47:50 +0300

    Update Finish.cs

--- MakingStairsToTheLove/Assets/0 Scripts/Finish.cs
+++ MakingStairsToTheLove/Assets/0 Scripts/Finish.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 public class Finish : MonoBehaviour, IInteractable
 {
+    bool triggered;
     void IInteractable.Interact(Player player)
     {
-        Observers.Instance.Notify_WinObservers();
+        if (triggered == false)
+            Observers.Instance.Notify_WinObservers();
+
+        triggered = true;
     }
 }
